8cc04463eeef7bb6105160009b09f70b9bfe32c2
Parantapa Bhattacharya <pb@parantapa.net>
2024-04-12 13:50:41 -0400

    Add preliminary completion support in language server

--- src/episim37/language_server.py
+++ src/episim37/language_server.py
@@ -1,23 +1,17 @@
 """Lnaguage server."""
 
 import logging
+from functools import cache
 from pathlib import Path
 
 import click
-from lsprotocol.types import (
-    DidOpenTextDocumentParams,
-    DidChangeTextDocumentParams,
-    TEXT_DOCUMENT_DID_OPEN,
-    TEXT_DOCUMENT_DID_CHANGE,
-    Diagnostic,
-    DiagnosticSeverity,
-    Range,
-    Position,
-)
+import tree_sitter as ts
+import lsprotocol.types as lsp
 from pygls.server import LanguageServer
 from platformdirs import user_log_dir
 
 from .misc import SourcePosition, EslError
+from .tree_sitter_bindings import get_parser, enum_query
 from .parse_tree import mk_pt, ParseTreeConstructionError
 from .ast import mk_ast
 from .check_ast import check_ast
@@ -25,38 +19,160 @@ from .codegen_openmp import simulator_str
 
 server = LanguageServer("esl37-server", "v0.1")
 
+_CURRENT_COMPLETIONS: list[lsp.CompletionItem] = []
+
 
 def error_to_diagnostic(
     type: str, explanation: str, pos: SourcePosition | None
-) -> Diagnostic:
+) -> lsp.Diagnostic:
     if pos is None:
-        range = Range(
-            start=Position(0, 0),
-            end=Position(0, 0),
+        range = lsp.Range(
+            start=lsp.Position(0, 0),
+            end=lsp.Position(0, 0),
         )
     else:
-        range = Range(
-            start=Position(pos.start[0], pos.start[1]),
-            end=Position(pos.end[0], pos.end[1]),
+        range = lsp.Range(
+            start=lsp.Position(pos.start[0], pos.start[1]),
+            end=lsp.Position(pos.end[0], pos.end[1]),
         )
-    return Diagnostic(
+    return lsp.Diagnostic(
         range=range,
-        severity=DiagnosticSeverity.Error,
+        severity=lsp.DiagnosticSeverity.Error,
         message=f"{type}: {explanation}",
         source="esl37-server",
     )
 
 
-@server.feature(TEXT_DOCUMENT_DID_OPEN)
-@server.feature(TEXT_DOCUMENT_DID_CHANGE)
-async def make_diagnostics(
-    ls: LanguageServer, params: DidOpenTextDocumentParams | DidChangeTextDocumentParams
+# fmt: off
+BUILTIN_TYPE_DOCS = [
+    # Integer types
+    ("int", "Integer"),
+    ("i8", "8 bit integer"),
+    ("i16", "16 bit integer"),
+    ("i32", "32 bit integer"),
+    ("i64", "64 bit integer"),
+
+    # Unsigned integer types
+    ("uint", "Unsigned integer"),
+    ("u8", "8 bit unsigned integer"),
+    ("u16", "16 bit unsigned integer"),
+    ("u32", "32 bit unsigned integer"),
+    ("u64", "64 bit unsigned integer"),
+
+    # Floating point types
+    ("float", "Floating point type"),
+    ("f32", "32 bit floating point type"),
+    ("f64", "64 bit floating point type"),
+
+    # Other types (that users are expected to write)
+    ("size", "Size type (unsigned)"),
+    ("bool", "Boolean type"),
+]
+
+BUILTIN_CONSTANT_DOCS = [
+    ("NUM_TICKS", "int", "Number of ticks"),
+    ("CUR_TICK", "int", "Current tick"),
+    ("NUM_NODES", "size", "Number of nodes"),
+    ("NUM_EDGES", "size", "Number of edges"),
+]
+
+BUILTIN_FUNCTION_DOCS = [
+    ("len", "len(nodeset | edgeset) : size", "Size of a nodeset or edgeset"),
+]
+# fmt: on
+
+
+@cache
+def builtin_completions() -> list[lsp.CompletionItem]:
+    completions: list[lsp.CompletionItem] = []
+    for type, doc in BUILTIN_TYPE_DOCS:
+        completions.append(
+            lsp.CompletionItem(
+                label=type,
+                kind=lsp.CompletionItemKind.Class,
+                detail="builtin type",
+                documentation=doc,
+            )
+        )
+
+    for const, detail, doc in BUILTIN_CONSTANT_DOCS:
+        completions.append(
+            lsp.CompletionItem(
+                label=const,
+                kind=lsp.CompletionItemKind.Constant,
+                detail=detail,
+                documentation=doc,
+            )
+        )
+
+    for func, detail, doc in BUILTIN_FUNCTION_DOCS:
+        completions.append(
+            lsp.CompletionItem(
+                label=func,
+                kind=lsp.CompletionItemKind.Function,
+                detail=detail,
+                documentation=doc,
+            )
+        )
+
+    return completions
+
+
+def enum_completions(root_node: ts.Node) -> list[lsp.CompletionItem]:
+    completions: list[lsp.CompletionItem] = []
+    for node, capture in enum_query().captures(root_node):
+        match capture:
+            case "enum_name":
+                completions.append(
+                    lsp.CompletionItem(
+                        label=node.text.decode(),
+                        kind=lsp.CompletionItemKind.Enum,
+                        detail="enumeration type",
+                    )
+                )
+            case "enum_const":
+                completions.append(
+                    lsp.CompletionItem(
+                        label=node.text.decode(),
+                        kind=lsp.CompletionItemKind.EnumMember,
+                        detail="enumeration constant",
+                    )
+                )
+
+    return completions
+
+
+def update_state(file_bytes: bytes | None):
+    _CURRENT_COMPLETIONS.clear()
+    _CURRENT_COMPLETIONS.extend(builtin_completions())
+
+    if file_bytes is not None:
+        parse_tree = get_parser().parse(file_bytes)
+        _CURRENT_COMPLETIONS.extend(enum_completions(parse_tree.root_node))
+
+
+@server.feature(
+    lsp.TEXT_DOCUMENT_COMPLETION, lsp.CompletionOptions(trigger_characters=[","])
+)
+def completions(params: lsp.CompletionParams | None = None) -> lsp.CompletionList:
+    """Returns completion items."""
+    return lsp.CompletionList(is_incomplete=False, items=_CURRENT_COMPLETIONS)
+
+
+@server.feature(lsp.TEXT_DOCUMENT_DID_OPEN)
+@server.feature(lsp.TEXT_DOCUMENT_DID_CHANGE)
+async def validate(
+    ls: LanguageServer,
+    params: lsp.DidOpenTextDocumentParams | lsp.DidChangeTextDocumentParams,
 ):
+    global _MOST_RECENT_PARSE_TREE
+
     ls.show_message_log("checking document")
 
     file_path = Path(params.text_document.uri)
     doc = ls.workspace.get_document(params.text_document.uri)
     file_bytes = doc.source.encode()
+    update_state(file_bytes)
 
     try:
         pt = mk_pt(str(file_path), file_bytes)
@@ -103,6 +219,7 @@ def io_server():
 
     logging.basicConfig(filename=str(log_file), filemode="a", level=logging.INFO)
 
+    update_state(None)
     server.start_io()
 
 
@@ -117,4 +234,5 @@ def tcp_server(host: str, port: int):
     """Start TCP/IP based server."""
     logging.basicConfig(filemode="a", level=logging.INFO)
 
+    update_state(None)
     server.start_tcp(host, port)
--- src/episim37/tree_sitter_bindings.py
+++ src/episim37/tree_sitter_bindings.py
@@ -1,8 +1,8 @@
 """Python bindings for the tree-sitter-parser."""
 
-
 from pathlib import Path
-from tree_sitter import Language, Parser
+from functools import cache
+from tree_sitter import Language, Parser, Query
 
 from platformdirs import user_cache_dir
 
@@ -10,7 +10,8 @@ LANGUAGE = "esl37"
 TREE_SITTER_DIR = Path(__file__).parent / f"tree-sitter-{LANGUAGE}"
 
 
-def get_parser() -> Parser:
+@cache
+def get_language() -> Language:
     state_dir = user_cache_dir(appname=LANGUAGE)
     state_dir = Path(state_dir)
     state_dir.mkdir(parents=True, exist_ok=True)
@@ -19,8 +20,24 @@ def get_parser() -> Parser:
 
     Language.build_library(str(library_file), [str(TREE_SITTER_DIR)])
     language = Language(str(library_file), LANGUAGE)
+    return language
 
+
+@cache
+def get_parser() -> Parser:
     parser = Parser()
-    parser.set_language(language)
+    parser.set_language(get_language())
 
     return parser
+
+
+@cache
+def enum_query() -> Query:
+    query = """
+    (enum
+        name: (identifier) @enum_name
+        const: (identifier) @enum_const
+     )
+    """
+    language = get_language()
+    return language.query(query)
