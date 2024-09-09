a2e9face0f52a1c704f91cd06c0d49f10d123541
Parantapa Bhattacharya <pb@parantapa.net>
2024-09-09 15:06:42 -0400

    Remove tree-sitter-code to separate repository

--- src/episim37/tree_sitter_bindings.py
+++ src/episim37/tree_sitter_bindings.py
@@ -1,41 +1,19 @@
 """Python bindings for the tree-sitter-parser."""
 
-import warnings
-
-warnings.simplefilter("ignore")
-
-from pathlib import Path
 from functools import cache
 from tree_sitter import Language, Parser, Query
 
-from platformdirs import user_cache_dir
-
-LANGUAGE = "esl37"
-TREE_SITTER_DIR = Path(__file__).parent / f"tree-sitter-{LANGUAGE}"
+import tree_sitter_esl as esl
 
 
 @cache
 def get_language() -> Language:
-    state_dir = user_cache_dir(appname=LANGUAGE)
-    state_dir = Path(state_dir)
-    state_dir.mkdir(parents=True, exist_ok=True)
-
-    library_file = state_dir / "languages.so"
-
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-
-        Language.build_library(str(library_file), [str(TREE_SITTER_DIR)])
-        language = Language(str(library_file), LANGUAGE)
-
-    return language
+    return Language(esl.language())
 
 
 @cache
 def get_parser() -> Parser:
-    parser = Parser()
-    parser.set_language(get_language())
-
+    parser = Parser(get_language())
     return parser
 
 
